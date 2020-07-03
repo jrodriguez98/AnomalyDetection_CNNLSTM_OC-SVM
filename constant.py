c5fee1515be9d5c8beec887a17dca05e60b7cd56
Jose Rodriguez <jrodriguez98@correo.ugr.es>
2020-07-03 19:02:52 +0200

    Add video paths

--- constant.py
+++ constant.py
@@ -1,7 +1,7 @@
 DATASETS_PATHS = dict(
-        hocky=dict(frames='data/raw_frames/hocky', model="models/hocky.h5"),
-        violentflow=dict(frames='data/raw_frames/violentflow', model="models/violentflow.h5"),
-        movies=dict(frames='data/raw_frames/movies', model="models/movies.h5")
+        hocky=dict(frames='data/raw_frames/hocky', model="models/hocky.h5", videos="data/raw_videos/HockeyFights"),
+        violentflow=dict(frames='data/raw_frames/violentflow', model="models/violentflow.h5", videos="data/raw_videos/violentflow"),
+        movies=dict(frames='data/raw_frames/movies', model="models/movies.h5", videos="data/raw_videos/movies")
     )
 
 FIX_LEN = 20
